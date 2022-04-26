a52a46618400ba2e9df15da3ab20d4d88726582c
Mezei László <h048855@stud.u-szeged.hu>
2022-04-27 00:06:06 +0200

    literalisan semmi uj

--- Mobilcsomag/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Mobilcsomag/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,19 +3,45 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    private static final String PREF=MainActivity.class.getPackage().toString();
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.name);
+        password= findViewById(R.id.pwd);
+
+
+        prefs=getSharedPreferences(PREF,MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor= prefs.edit();
+        editor.putString("username",username.getText().toString());
+        editor.putString("password",password.getText().toString());
+        editor.apply();
     }
 
     public void register(View view) {
         Intent intent = new Intent(this,Reg.class);
         startActivity(intent);
+    }
+
+    public void login(View view) {
+        String Name = username.getText().toString();
+        String Password = password.getText().toString();
     }
 }
--- Mobilcsomag/app/src/main/java/com/example/myapplication/Reg.java
+++ Mobilcsomag/app/src/main/java/com/example/myapplication/Reg.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Reg extends AppCompatActivity {
+    EditText username;
+    EditText pwd;
+    private static final String PREF = MainActivity.class.getPackage().toString();
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+
+        username = findViewById(R.id.reg_name);
+        pwd = findViewById(R.id.reg_pwd);
+
+        prefs = getSharedPreferences(PREF, MODE_PRIVATE);
+        String userName = prefs.getString("username","");
+        String password = prefs.getString("password","");
+
+        username.setText(userName);
+        pwd.setText(password);
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    public void register(View view) {
     }
 }
